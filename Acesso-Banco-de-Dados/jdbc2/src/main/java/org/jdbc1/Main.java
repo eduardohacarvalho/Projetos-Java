aa86058e8e0831925af7622cbb41e35a974f8035
Eduardo Carvalho <eduardohacarvalho@icloud.com>
2026-03-30 20:58:28 -0300

    inserindo dados no banco de dados

--- Acesso-Banco-de-Dados/jdbc2/src/main/java/org/jdbc1/Main.java
+++ Acesso-Banco-de-Dados/jdbc2/src/main/java/org/jdbc1/Main.java
@@ -1,14 +1,55 @@
 package org.jdbc1;
 
 import org.jdbc1.db.DB;
+import org.jdbc1.db.DbException;
 
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Main {
     static void main() {
-
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection conn = null;
-        Statement st = null;
+        PreparedStatement st= null;
+
+        try {
+            conn = DB.getConnection();
+            /*
+            st = conn.prepareStatement(
+                    "INSERT INTO seller"
+                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+                    + "VALUES "
+                    + "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            st.setString(1, "Julia Silva");
+            st.setString(2, "julia@gmail.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("25/05/1985").getTime()));
+            st.setDouble(4, 3000.0);
+            st.setInt(5, 4);
+*/
+            st = conn.prepareStatement(
+                    "insert into department (Name) values ('D1'),('D2')",
+                    Statement.RETURN_GENERATED_KEYS);
+            int rowsAffected = st.executeUpdate();
+            if (rowsAffected > 0 ){
+                ResultSet resultSet = st.getGeneratedKeys();
+                while (resultSet.next()){
+                    int id = resultSet.getInt(1);
+                    System.out.println("Feito! Id registrado: " + id);
+
+                }
+            } else {
+                System.out.println("Nenhuma linha foi alterada no banco de dados");
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+/*        Statement st = null;
         ResultSet rs = null;
 
         try {
@@ -27,7 +68,7 @@ public class Main {
             DB.closeStatement(st);
             DB.closeConnection();
         }
-
+*/
 
     }
 }
