24a2829599b2aea7fa01af575a34d73ecfeabc68
Eduardo Carvalho <eduardohacarvalho@icloud.com>
2026-04-01 00:21:26 -0300

    Implementando Many to Many

--- jpa-spring/src/main/java/com/jpa_spring/jpa_spring/entities/Category.java
+++ jpa-spring/src/main/java/com/jpa_spring/jpa_spring/entities/Category.java
@@ -1,5 +1,6 @@
 package com.jpa_spring.jpa_spring.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,6 +17,8 @@ public class Category implements Serializable {
     private Integer id;
     private String name;
 
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category(Integer id, String name) {
