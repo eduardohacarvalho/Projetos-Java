709a3dafb08b4cf6aa0cc1899f65d3212ecd7c51
Eduardo Carvalho <eduardohacarvalho@icloud.com>
2026-04-02 00:16:56 -0300

    Criando xadrez e implementando classe de posicao

---
+++ sistema-xadrez/src/main/java/com/projeto_java/Sistema_Xadrez/SistemaXadrezApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_java.Sistema_Xadrez;
+
+import com.projeto_java.Sistema_Xadrez.boardgame.Position;
+
+public class SistemaXadrezApplication {
+
+	public static void main(String[] args) {
+
+    Position pos = new Position(2,3);
+        System.out.println(pos);
+	}
+
+}
---
+++ sistema-xadrez/src/main/java/com/projeto_java/Sistema_Xadrez/boardgame/Position.java
@@ -0,0 +1,35 @@
+package com.projeto_java.Sistema_Xadrez.boardgame;
+
+public class Position {
+    private Integer row;
+    private Integer column;
+
+    public Position(Integer row, Integer column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public Position() {
+    }
+
+    public Integer getRow() {
+        return row;
+    }
+
+    public void setRow(Integer row) {
+        this.row = row;
+    }
+
+    public Integer getColumn() {
+        return column;
+    }
+
+    public void setColumn(Integer column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return "Posição {"+ row + ", " + column + '}';
+    }
+}
