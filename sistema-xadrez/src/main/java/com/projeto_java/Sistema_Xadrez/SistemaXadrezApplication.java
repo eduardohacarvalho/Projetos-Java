a4e33611c9a727f677c80c36192c01a16d768601
Eduardo Carvalho <eduardohacarvalho@icloud.com>
2026-04-02 00:25:50 -0300

    Implementando Board e Piece

--- sistema-xadrez/src/main/java/com/projeto_java/Sistema_Xadrez/SistemaXadrezApplication.java
+++ sistema-xadrez/src/main/java/com/projeto_java/Sistema_Xadrez/SistemaXadrezApplication.java
@@ -1,13 +1,13 @@
 package com.projeto_java.Sistema_Xadrez;
 
+import com.projeto_java.Sistema_Xadrez.boardgame.Board;
 import com.projeto_java.Sistema_Xadrez.boardgame.Position;
 
 public class SistemaXadrezApplication {
 
 	public static void main(String[] args) {
 
-    Position pos = new Position(2,3);
-        System.out.println(pos);
+    Board board = new Board(8, 8);
 	}
 
 }
