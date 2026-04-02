4d6647e534c3e93466807508208425df0e3c8d4c
Eduardo Carvalho <eduardohacarvalho@icloud.com>
2026-04-02 20:47:14 -0300

    Tratando exceções e limpando a tela

--- sistema-xadrez/src/main/java/com/projeto_java/Sistema_Xadrez/SistemaXadrezApplication.java
+++ sistema-xadrez/src/main/java/com/projeto_java/Sistema_Xadrez/SistemaXadrezApplication.java
@@ -1,10 +1,12 @@
 package com.projeto_java.Sistema_Xadrez;
 
 
+import com.projeto_java.Sistema_Xadrez.chess.ChessException;
 import com.projeto_java.Sistema_Xadrez.chess.ChessMatch;
 import com.projeto_java.Sistema_Xadrez.chess.ChessPiece;
 import com.projeto_java.Sistema_Xadrez.chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class SistemaXadrezApplication {
@@ -16,16 +18,22 @@ public class SistemaXadrezApplication {
         UI.printBoard(chessMatch.getPieces());
 
         while (true) {
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
 
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
 	}
 
