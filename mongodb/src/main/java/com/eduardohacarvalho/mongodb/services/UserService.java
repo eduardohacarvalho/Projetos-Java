ef7e59fb9a17cf0032cd18f7e8d24b60f230fa0d
Eduardo Carvalho <eduardohacarvalho@icloud.com>
2026-04-04 18:23:52 -0300

    obtendo usuario por id e tratando excecoes

--- mongodb/src/main/java/com/eduardohacarvalho/mongodb/services/UserService.java
+++ mongodb/src/main/java/com/eduardohacarvalho/mongodb/services/UserService.java
@@ -2,10 +2,13 @@ package com.eduardohacarvalho.mongodb.services;
 
 import com.eduardohacarvalho.mongodb.domain.User;
 import com.eduardohacarvalho.mongodb.repository.UserRepository;
+import com.eduardohacarvalho.mongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
     @Autowired
@@ -13,5 +16,10 @@ public class UserService {
 
     public List<User> findAll(){
         return  repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
