411abcaad3078cf8fd63ea177207d719403abde8
Eduardo Carvalho <eduardohacarvalho@icloud.com>
2026-04-04 18:03:19 -0300

    usando DTO para retornar usuario

--- mongodb/src/main/java/com/eduardohacarvalho/mongodb/resources/UserResource.java
+++ mongodb/src/main/java/com/eduardohacarvalho/mongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.eduardohacarvalho.mongodb.resources;
 
 import com.eduardohacarvalho.mongodb.domain.User;
+import com.eduardohacarvalho.mongodb.dto.UserDTO;
 import com.eduardohacarvalho.mongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -20,9 +22,11 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method= RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll () {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        System.out.println("DEBUG: Quantidade de usuários no banco: " + list.size());
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream()
+                .map(x -> new UserDTO(x))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
