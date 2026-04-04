c29f880d482037295a3135d88f45890cb61b2324
Eduardo Carvalho <eduardohacarvalho@icloud.com>
2026-04-04 20:41:37 -0300

    Endpoint retorna posts de um usuario e posts por id

---
+++ mongodb/src/main/java/com/eduardohacarvalho/mongodb/resources/PostResource.java
@@ -0,0 +1,30 @@
+package com.eduardohacarvalho.mongodb.resources;
+
+import com.eduardohacarvalho.mongodb.domain.Post;
+import com.eduardohacarvalho.mongodb.domain.User;
+import com.eduardohacarvalho.mongodb.dto.UserDTO;
+import com.eduardohacarvalho.mongodb.services.PostService;
+import com.eduardohacarvalho.mongodb.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value="/{id}", method= RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+}
---
+++ mongodb/src/main/java/com/eduardohacarvalho/mongodb/services/PostService.java
@@ -0,0 +1,21 @@
+package com.eduardohacarvalho.mongodb.services;
+
+import com.eduardohacarvalho.mongodb.domain.Post;
+import com.eduardohacarvalho.mongodb.repository.PostRepository;
+import com.eduardohacarvalho.mongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
